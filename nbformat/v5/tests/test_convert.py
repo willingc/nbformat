ab35d8558bfe3c92ba668762a2161f2ca7653be1
Carol Willing <carolcode@willingconsulting.com>
2019-02-06 14:47:07 -0800

    add test updates

--- nbformat/v5/tests/test_convert.py
+++ nbformat/v5/tests/test_convert.py
@@ -5,66 +5,52 @@ from nbformat import validate
 from .. import convert
 
 from . import nbexamples
-from nbformat.v3.tests import nbexamples as v3examples
-from nbformat import v3, v4
+from nbformat.v4.tests import nbexamples as v4examples
+from nbformat import v4, v5
+
 
 def test_upgrade_notebook():
-    nb03 = copy.deepcopy(v3examples.nb0)
-    validate(nb03)
-    nb04 = convert.upgrade(nb03)
+    nb04 = copy.deepcopy(v4examples.nb0)
     validate(nb04)
+    nb05 = convert.upgrade(nb04)
+    validate(nb05)
+
 
 def test_downgrade_notebook():
-    nb04 = copy.deepcopy(nbexamples.nb0)
+    nb05 = copy.deepcopy(nbexamples.nb0)
+    validate(nb05)
+    nb04 = convert.downgrade(nb05)
     validate(nb04)
-    nb03 = convert.downgrade(nb04)
-    validate(nb03)
+
 
 def test_upgrade_heading():
-    v3h = v3.new_heading_cell
-    v4m = v4.new_markdown_cell
-    for v3cell, expected in [
+    v4h = v4.new_heading_cell
+    v5m = v5.new_markdown_cell
+    for v4cell, expected in [
+        (v4h(source="foo", level=1), v5m(source="# foo")),
         (
-            v3h(source='foo', level=1),
-            v4m(source='# foo'),
+            v4h(source="foo\nbar\nmulti-line\n", level=4),
+            v5m(source="#### foo bar multi-line"),
         ),
         (
-            v3h(source='foo\nbar\nmulti-line\n', level=4),
-            v4m(source='#### foo bar multi-line'),
-        ),
-        (
-            v3h(source=u'ünìcö∂e–cønvërsioñ', level=4),
-            v4m(source=u'#### ünìcö∂e–cønvërsioñ'),
+            v4h(source=u"ünìcö∂e–cønvërsioñ", level=4),
+            v5m(source=u"#### ünìcö∂e–cønvërsioñ"),
         ),
     ]:
         upgraded = convert.upgrade_cell(v3cell)
         assert upgraded == expected
 
+
 def test_downgrade_heading():
-    v3h = v3.new_heading_cell
-    v4m = v4.new_markdown_cell
-    v3m = lambda source: v3.new_text_cell('markdown', source)
-    for v4cell, expected in [
-        (
-            v4m(source='# foo'),
-            v3h(source='foo', level=1),
-        ),
-        (
-            v4m(source='#foo'),
-            v3h(source='foo', level=1),
-        ),
-        (
-            v4m(source='#\tfoo'),
-            v3h(source='foo', level=1),
-        ),
-        (
-            v4m(source='# \t  foo'),
-            v3h(source='foo', level=1),
-        ),
-        (
-            v4m(source='# foo\nbar'),
-            v3m(source='# foo\nbar'),
-        ),
+    v4h = v4.new_heading_cell
+    v5m = v5.new_markdown_cell
+    v4m = lambda source: v4.new_text_cell("markdown", source)
+    for v5cell, expected in [
+        (v5m(source="# foo"), v4h(source="foo", level=1)),
+        (v5m(source="#foo"), v4h(source="foo", level=1)),
+        (v5m(source="#\tfoo"), v4h(source="foo", level=1)),
+        (v5m(source="# \t  foo"), v4h(source="foo", level=1)),
+        (v5m(source="# foo\nbar"), v4m(source="# foo\nbar")),
     ]:
-        downgraded = convert.downgrade_cell(v4cell)
+        downgraded = convert.downgrade_cell(v5xcell)
         assert downgraded == expected
